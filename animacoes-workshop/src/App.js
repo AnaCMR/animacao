729088fe9acb7a132b2298100d14ac645a0f3e6f
AnaCMR <ana.silva@bandtec.com.br>
2022-06-01 19:40:38 -0300

    adding final final component

--- animacoes-workshop/src/App.js
+++ animacoes-workshop/src/App.js
@@ -4,6 +4,7 @@ import Exemplo from './components/Exemplos/exemplos';
 import Banner from './components/Banner/banner';
 import Introducao from './components/Introducao/introducao';
 import Conselhos from './components/Conselhos/conselhos';
+import Thanks from './components/Agradecimentos/thanks';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Introducao/>
       <Exemplo/>
       <Conselhos/>
+      <Thanks/>
     </div>
   );
 }
